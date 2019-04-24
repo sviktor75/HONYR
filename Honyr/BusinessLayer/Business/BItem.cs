3c60c0bc1ba3c79b97b94de09aded064226e1ac5
tothalexlaszlo <48099791+tothalexlaszlo@users.noreply.github.com>
2019-04-24 19:13:41 +0200

    .

---
+++ Honyr/BusinessLayer/Business/BItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Operations;
+
+namespace BusinessLayer.Business
+{
+    public class BItem
+    {
+        OItem itemOP = new OItem();
+
+
+        public int AddItem(bool active, string deviceID, string deviceName, string ip, string mac, long locationid, int symbolId, string description)
+        {
+            return itemOP.AddItem(active,deviceID,deviceName,ip,mac,locationid,symbolId,description);
+
+        }
+
+        public int ModItem(long id, bool active, string deviceID, string deviceName, string ip, string mac, long locationid, int symbolId, string description)
+        {
+            return itemOP.ModItem(id, active, deviceID, deviceName, ip, mac, locationid, symbolId, description);
+        }
+
+        public int DelItem(long id)
+        {
+            return itemOP.DelItem(id);
+        }
+
+        public List<String> GetItem()
+        {
+            return itemOP.GetItem();
+        }
+
+        public List<String> GetItemByName(string name)
+        {
+            return itemOP.GetItemByName(name);
+        }
+    }
+}
--- Honyr/DataLayer/Operations/OItem.cs
+++ Honyr/DataLayer/Operations/OItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,28 +9,92 @@ namespace DataLayer.Operations
 {
     public class OItem
     {
-        public void AddItem()
+        OInitDataConnection conn = new OInitDataConnection();
+
+        public int AddItem(bool active, string deviceID, string deviceName, string ip, string mac, long locationid, int symbolId, string description)
         {
+            string query = "INSERT INTO location (active, deviceID, deviceName, ip, mac, locationid, symbolid, description)" + " VALUES" + " ('" + active + "', '" + deviceID + "', '" + deviceName + "', '" + ip + "', '" + mac + "', '" + locationid + "','" + symbolId + "','" + description + "');";
+
+            conn.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn.conn);
+            int effectedRows = cmd.ExecuteNonQuery();
+            conn.CloseConnection();
+            return effectedRows;
         }
 
-        public void DelItem()
+
+        public int ModItem(long id, bool active, string deviceID, string deviceName, string ip, string mac, long locationid, int symbolId, string description)
         {
+            string query = "update item set active='" + active + "', deviceID='" + deviceID + "', deviceName='" + deviceName + "', ip='" + ip + "', mac='" + mac + "', locationid='" + locationid + "', symbolid='" + symbolId + "', description='" + description + "' where id='" + id + "';";
+
+            conn.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn.conn);
+            int effectedRows = cmd.ExecuteNonQuery();
+            conn.CloseConnection();
+            return effectedRows;
+        }
+
+        public int DelItem(long id)
+        {
+            string query = "delete from item where id='" + id + "';";
+
+            conn.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn.conn);
+            int effectedRows = cmd.ExecuteNonQuery();
+            conn.CloseConnection();
+            return effectedRows;
+        }
+
+        public List<String> GetItem()
+        {
+            string query = "select deviceID from item;";
+
+            List<string> retList = new List<string>();
+
+            conn.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn.conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                retList.Add(dataReader["deviceID"] + "");
+            }
+            dataReader.Close();
+            conn.CloseConnection();
+
+            return retList;
+        }
+
+
+        public List<String> GetItemByName(string name)
+        {
+            string query = "select * from item where name like '" + name + "' order by name limit 1;";
+
+            List<string> retList = new List<string>();
+
+            conn.OpenConnection();
+            MySqlCommand cmd = new MySqlCommand(query, conn.conn);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+
+            while (dataReader.Read())
+            {
+                retList.Add(dataReader["id"] + "");
+                retList.Add(dataReader["active"] + "");
+                retList.Add(dataReader["deviceID"] + "");
+                retList.Add(dataReader["deviceName"] + "");
+                retList.Add(dataReader["ip"] + "");
+                retList.Add(dataReader["mac"] + "");
+                retList.Add(dataReader["locationid"] + "");
+                retList.Add(dataReader["symbolid"] + "");
+                retList.Add(dataReader["description"] + "");
+            }
+            dataReader.Close();
+            conn.CloseConnection();
+
+            return retList;
 
         }
 
-        public void ModItem()
-        {
-
-        }
-
-        public void GetById()
-        {
-
-        }
-
-        public void GetByName()
-        {
-
-        }
     }
 }
